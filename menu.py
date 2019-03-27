f296a4a3afbad5d70373a664f5131aad3cdd426e
Mark <wernsdorfer@gmail.com>
2019-03-27 16:01:47 +0100

    first version of shutter signal

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 from json import JSONDecodeError
 
 import RPi.GPIO
@@ -53,9 +54,11 @@ class IRControl:
 
     @staticmethod
     def send_signal():
-        RPi.GPIO.output(IRControl.ir_channel, False)
-        time.sleep(1.)
-        RPi.GPIO.output(IRControl.ir_channel, True)
+        #RPi.GPIO.output(IRControl.ir_channel, False)
+        #time.sleep(1.)
+        #RPi.GPIO.output(IRControl.ir_channel, True)
+
+        subprocess.call("irsend SEND_ONCE Nikon2 shutter", shell=True)
         time.sleep(1.)
 
 
